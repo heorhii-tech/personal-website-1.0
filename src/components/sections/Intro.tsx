c35d1d9795e2bfb6fdd89c2707b8ad93cce788f2
Heorhii <heorhiimadzharov@gmail.com>
2025-02-08 13:01:29 +0100

    ..

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { IntroConfig } from "../../share/common/types";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -6,31 +7,37 @@ interface IntroProps {
   config: IntroConfig;
 }
 const Intro = ({ config }: IntroProps) => {
-
   return (
-    
-      <div className="section-intro__wrapper">
-        <div className="section-intro__content">
-          <h1 className="section-intro__title">{config.title}</h1>
-          <p className="section-intro__description">{config.description}</p>
-          <div className="section-intro__buttons">
-            <Button style={{width: "150px"}} >
-             {config.primaryButtonLink&& <Link className="" href={config.primaryButtonLink}>
+    <div className="section-intro__wrapper">
+      <div className="section-intro__content">
+        <h1 className="section-intro__title">{config.title}</h1>
+        <p className="section-intro__description">{config.description}</p>
+        <div className="section-intro__buttons">
+          <Button style={{ width: "150px" }}>
+            {config.primaryButtonLink && (
+              <Link className="" href={config.primaryButtonLink}>
                 {config.primaryButtonTitle}
-              </Link>}
-            </Button>
-            <Button style={{width: "150px"}} variant="secondary">
-              {config.secondaryButtonLink&& <Link className="" href={config.secondaryButtonLink}>
+              </Link>
+            )}
+          </Button>
+          <Button style={{ width: "150px" }} variant="secondary">
+            {config.secondaryButtonLink && (
+              <Link className="" href={config.secondaryButtonLink}>
                 {config.secondaryButtonTitle}
-              </Link>}
-            </Button>
-          </div>
-        </div>
-        <div className="section-intro__media">
-          <img className="section-intro__image" src={config.image} alt="" />
+              </Link>
+            )}
+          </Button>
         </div>
       </div>
-   
+      <div className="section-intro__media">
+        <Image
+          className="section-intro__image"
+          src={config.image}
+          alt=""
+         
+        />
+      </div>
+    </div>
   );
 };
 export default Intro;
