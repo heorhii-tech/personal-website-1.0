730c9f77d0c2436db29cd802c8c63b9a7034bb5b
Heorhii <heorhiimadzharov@gmail.com>
2025-02-10 18:44:03 +0100

    ..///

--- src/components/animations/MatrixEffect.tsx
+++ src/components/animations/MatrixEffect.tsx
@@ -1,16 +1,32 @@
 'use client';
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { useMediaQuery } from "@uidotdev/usehooks";
 
-const MatrixEffect = () => {
-  const isLargeScreen = useMediaQuery("(min-width: 1280px)");
-  const [columns, setColumns] = useState([]);
+
+interface Column {
+  id: number;
+  characters: string[];
+  delay: number;
+}
+
+const MatrixEffect: React.FC = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+useEffect(() => {
+  const mediaQuery = window.matchMedia("(min-width: 1280px)");
+  setIsLargeScreen(mediaQuery.matches);
+
+  const handleChange = (event: MediaQueryListEvent) => setIsLargeScreen(event.matches);
+  mediaQuery.addEventListener("change", handleChange);
+
+  return () => mediaQuery.removeEventListener("change", handleChange);
+}, []);
+  const [columns, setColumns] = useState<Column[]>([]);
   const columnCount = isLargeScreen ? 34 : 25; // Количество столбцов
   const speed = 3; // Скорость падения
 
   useEffect(() => {
-    const generateColumns = () => {
+    const generateColumns = (): Column[] => {
       return new Array(columnCount).fill(0).map((_, index) => ({
         id: index,
         characters: new Array(15) // Длина падающих символов
@@ -21,12 +37,12 @@ const MatrixEffect = () => {
     };
 
     setColumns(generateColumns());
-  }, []);
+  }, [columnCount]);
 
   return (
-    <div className=" hidden lg:flex w-full  bg-black overflow-hidden flex absolute top-0 left-0 z-[-1] mt-[25px] xl:mt-[100px]">
+    <div className="hidden lg:flex w-full bg-black overflow-hidden flex absolute top-0 left-0 z-[-1] mt-[25px] xl:mt-[100px]">
       {columns.map((column) => (
-        <div key={column.id} className=" flex flex-col mx-1">
+        <div key={column.id} className="flex flex-col mx-1">
           {column.characters.map((char, index) => (
             <motion.span
               key={index}
--- src/share/layout/Header.tsx
+++ src/share/layout/Header.tsx
@@ -1,7 +1,6 @@
 // components/Header.js
 "use client";
-import React from "react";
-import { useMediaQuery } from "@uidotdev/usehooks";
+import React, { useEffect, useState } from "react";
 import { HeaderConfig } from "../common/types";
 import HeaderNav from "./HeaderNav";
 import BurgerMenu from "./BurgerMenu";
@@ -9,15 +8,31 @@ import BurgerMenu from "./BurgerMenu";
 interface Props {
   logo: React.ReactNode;
   config: HeaderConfig;
-
 }
-export default function Header({ logo, config,  }: Props) {
-const isLargeScreen = useMediaQuery("(min-width: 1024px)");
+
+export default function Header({ logo, config }: Props) {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+    setIsLargeScreen(mediaQuery.matches);
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsLargeScreen(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
   return (
     <header className="container header-wrapper">
       {logo}
-     
-      {isLargeScreen ? <HeaderNav navigation={config.navigation} /> : <BurgerMenu navigation={config.navigation} />}
+      {isLargeScreen ? (
+        <HeaderNav navigation={config.navigation} />
+      ) : (
+        <BurgerMenu navigation={config.navigation} />
+      )}
     </header>
   );
 }
